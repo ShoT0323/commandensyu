a91877d210c8bc9e1129ebc87fba977baaa06885
koda-ma <r.kodama.sys20@morijyobi.ac.jp>
2022-11-08 12:35:02 +0900

    C³®¹

--- Main.java
+++ Main.java
@@ -2,14 +2,21 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[]ager){
         Scanner sc = new Scanner(System.in);
-	System.out.print("体重:");
-	double weigth = sc.nextDouble();
-	System.out.print("身長:");
-        double heigth = sc.nextDouble();
+	System.out.print("一人目の体重:");
+	double weigth1 = sc.nextDouble();
+	System.out.print("一人目の身長:");
+        double heigth1 = sc.nextDouble();
+	System.out.print("二人目の体重:");
+	double weigth2 = sc.nextDouble();
+	System.out.print("二人目の身長:");
+	double heigth2 = sc.nextDouble();
 
-        double bmi = weigth /(heigth*heigth);
+        double bmi1 = weigth1 /(heigth1*heigth1);
+	double bmi2 = weigth2 /(heigth2*heigth2);
 
-	System.out.print("あなたのBMIは");
-        System.out.println((Math.round(bmi * 100)/100.0)+"です");
+	System.out.print("一人目のBMIは");
+        System.out.println((Math.round(bmi1 * 100)/100.0)+"です");
+	System.out.print("二人目のBMIは");
+	System.out.println((Math.round(bmi2 * 100)/100.0)+"です");
     }
 }
